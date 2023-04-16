533e1b7f655832c2858fd55c533263849d44bfde
Jahnavi Boda <120732253+JahnaviBoda6@users.noreply.github.com>
2023-04-16 21:44:45 +0530

    Update Boat.java

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,8 +1,12 @@
 package com.driver;
 
 public class Boat implements WaterVehicle {
-    String name;
-    int capacity;
+    private String name;
+    private int capacity;
+    public Boat(String name, int capacity){
+        this.name=name;
+        this.capacity=capacity;
+    }
     public String getVehicleName(){
         return name;
     }
